9360a6777e79bf2fdda902c067564c96d76a9754
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-04-02 23:36:49 -0300

    :sparkles: implementação de rotas

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,61 +1,30 @@
+
 function Home() {
     return (
         <div
-            style={{
-                backgroundColor: "#312e81",
-                display: "flex",
-                justifyContent: "center"
-            }}
+           className="bg-indigo-900 flex justify-center"
         >
             <div 
-             style={{
-                display: "grid",
-                gridTemplateColumns: "1fr 1fr",
-                color: 'white',
-                width: '100%',
-                maxWidth: '1280px'
-             }}
+             className="container grid grid-cols-2 text-white"
             >
                 <div 
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: "1rem",
-                    paddingTop: "1rem",
-                    paddingBottom: "1rem"
-                }}
+              className="flex flex-col justify-center items-center "
                 >
                         <h2 
-                        style={{
-                            fontSize: "3rem",
-                            fontWeight: "bold"
-                        }}
+                      className="text-5xl font-bold"
                         >Seja Bem Vindo!
                         </h2>
                         <p
-                        style={{
-                            fontSize: "1.25rem"
-                        }}
+                       className="text-xl"
                         >
                         Expresse aqui seus pensamentos e opiniões
                         </p>
 
                         <div
-                         style={{
-                            display: "flex",
-                            justifyContent: "space-around",
-                            gap: "1rem"
-                         }}
+                       className="flex justify-around gap-4"
                         >
                             <div
-                            style={{
-                                borderRadius: "0.5rem",
-                                color: "white",
-                                border: "2px solid white",
-                                padding: "0.5rem 1rem"
-                            }}
+                            className="rounded border-white border-solid border-2 py-2 px-4"
                             >
                                 Nova Postagem
                             </div>
@@ -64,16 +33,11 @@ function Home() {
                 </div>
 
                 <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center"
-                }}
+                className="flex justify-center"
                 >
                     <img src="https://i.imgur.com/fyfri1v.png"
                         alt="Imagem Página Home"
-                        style={{
-                            width: "60%"
-                        }}
+                        className="w-2/3"
                     />
                 </div>
 
