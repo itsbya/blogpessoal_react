2f4306968bde98f93d4e963d5d2dafe5d330369f
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-04-06 19:14:55 -0300

    :sparkles: Conexão com backend

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,46 @@
-import { Link } from "react-router-dom"
-
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
-  return (
-    <>
-      <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
-        <div className="container flex justify-between text-lg mx-8">
-        <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-          <div className="flex gap-4">
-            Postagens
-            Temas
-            Cadastrar tema
-            Perfil
-            Sair
-          </div>
-          </div>
-      </div>
-    </>
-  )
+    // Objeto responsável por redirecionar o usuário para uma outra rota
+    const navigate = useNavigate();
+
+    // Consumo do Contexto AuthContext 
+    // usamos a desestruturação para selecionar apenas o que precisamos
+    const { handleLogout } = useContext(AuthContext);
+
+    function logout(){
+        handleLogout();
+        alert('O Usuário foi desconectado com sucesso!');
+        navigate("/")
+    }
+
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg mx-8">
+                    
+                    <Link to="/home" className="text-2xl font-bold">
+                        Blog Pessoal
+                    </Link>
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        <Link to='' onClick={logout} className="hover:underline">
+                            Sair
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
